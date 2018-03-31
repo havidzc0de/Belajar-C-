f9a556fe9cb9ad61c728a297fc67977c54f2a5d8
havidzc0de <havidzc0de@gmail.com>
2018-03-31 15:34:33 +0700

    Iseng bintang

--- Praktek/iseng.cpp
+++ Praktek/iseng.cpp
@@ -4,7 +4,45 @@ using namespace std;
 
 int main(){
 	char bintang[12][12];
+
 	for(int i=0; i<11; i++){
+	  	for(int j=0; j<i; j++){
+	    	bintang[i][j] = '*';
+	    	cout << bintang[i][j];
+	  	}
+	cout << endl;
+	}
+
+	cout <<endl;
+	for (int i=11; i>0; i--){
+		for(int j=0; j<i; j++){
+		bintang[i][j] = '*';
+		cout << bintang[i][j];
+		}
+	cout << endl;
+	}
+	
+
+	cout << endl;
+	for(int i=0; i<11; i++){
+		for(int j=11; j>i; j--){
+		bintang[i][j] = ' ';
+		cout << bintang[i][j];
+		}
+		for(int j=0; j<i; j++){
+		bintang[i][j] = '*';
+		cout << bintang[i][j];
+		}
+	cout << endl;
+	}
+
+
+	cout << endl;
+	for(int i=0; i<11; i++){
+		for(int j=0; j<i; j++){
+		bintang[i][j] = ' ';
+		cout << bintang[i][j];
+		}
 		for(int j=11; j>i; j--){
 		bintang[i][j] = '*';
 		cout << bintang[i][j];
